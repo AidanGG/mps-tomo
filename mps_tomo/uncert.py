d7a4b18483c3e16811dfa4c0abbbf55e0eddaabe
Aidan Dang <dang@aidan.gg>
2019-11-19 19:55:05 +1100

    R_hat matrix

--- mps_tomo/uncert.py
+++ mps_tomo/uncert.py
@@ -1,5 +1,23 @@
+from functools import reduce
+
 import numpy as np
 
 
 def pauli_proj(dens_mat, pauli_prod):
     return np.trace(dens_mat @ pauli_prod) * pauli_prod / np.size(dens_mat, 0)
+
+
+def R_hat(sigmas):
+    sigma_size = np.size(sigmas[0], axis=0)
+    dim = sigma_size * (2 ** (len(sigmas) - 1))
+
+    def kron_sigma(qubit, sigma):
+        left_size = 2 ** qubit
+        right_size = dim // (left_size * sigma_size)
+        left, right = (
+            np.eye(left_size, dtype=np.complex128),
+            np.eye(right_size, dtype=np.complex128),
+        )
+        return np.kron(np.kron(left, sigma), right)
+
+    return reduce(np.add, (kron_sigma(*i) for i in enumerate(sigmas)))
--- tests/test_uncert.py
+++ tests/test_uncert.py
@@ -4,8 +4,8 @@ import numpy as np
 from numpy.testing import assert_allclose
 from qiskit.quantum_info import pauli_group, random_density_matrix
 
-import mps_tomo.uncert as uncert
-import mps_tomo.utils
+from mps_tomo.uncert import pauli_proj, R_hat
+from mps_tomo.utils import pauli_group
 
 
 def test_pauli_proj():
@@ -15,8 +15,22 @@ def test_pauli_proj():
 
     dens_mat = random_density_matrix(2 ** N_QUBITS, seed=SEED)
     reconstructed = reduce(
-        np.add,
-        (uncert.pauli_proj(dens_mat, p) for p in mps_tomo.utils.pauli_group(N_QUBITS)),
+        np.add, (pauli_proj(dens_mat, p) for p in pauli_group(N_QUBITS))
     )
 
     assert_allclose(reconstructed, dens_mat, rtol=0.0, atol=TOL)
+
+
+def test_R_hat():
+    TOL = 100 * np.spacing(np.complex128(1.0).real)
+    np.random.seed(7777)
+
+    sigmas = [np.random.randn(4, 4) + 1j * np.random.randn(4, 4) for i in range(3)]
+
+    example = (
+        np.kron(sigmas[0], np.eye(4))
+        + np.kron(np.kron(np.eye(2), sigmas[1]), np.eye(2))
+        + np.kron(np.eye(4), sigmas[2])
+    )
+
+    assert_allclose(R_hat(sigmas), example, rtol=0.0, atol=TOL)
