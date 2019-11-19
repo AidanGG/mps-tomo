8d88aed7dc013042b3433631b327123da346abd3
Aidan Dang <dang@aidan.gg>
2019-11-20 02:55:34 +1100

    Rework iteration

--- mps_tomo/uncert.py
+++ mps_tomo/uncert.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 import numpy as np
 
 from mps_tomo.utils import kron_embed, pauli_group
@@ -15,22 +13,24 @@ def R_hat(sigmas, num_qubits):
 
 def iteration(k, sigmas, num_qubits, max_its=100, delta=0.1):
     dim = 2 ** num_qubits
+    paulis = pauli_group(k)
 
     R = R_hat(sigmas, num_qubits)
 
     Y = np.zeros((dim, dim), dtype=np.complex128)
-
-    for i in range(max_its):
+    for _ in range(max_its):
         w, v = np.linalg.eigh(Y)
         y_val, y_vec = w[-1], v[:, -1]
 
-        paulis = (
-            kron_embed(q, p, num_qubits)
-            for p in pauli_group(k)
-            for q in range(num_qubits - k + 1)
-        )
+        X = np.zeros_like(Y)
+        for qubit in range(num_qubits - k + 1):
+            y_vec_reshaped = np.reshape(y_vec, (2 ** qubit, 2 ** k, -1))
+            for p in paulis:
+                X += np.dot(
+                    y_vec.conj(), np.ravel(np.einsum("ij,ajb->aib", p, y_vec_reshaped))
+                ) * kron_embed(qubit, p, num_qubits)
+        X *= y_val / dim
 
-        X = y_val / dim * reduce(np.add, (y_vec.conj() @ p @ y_vec * p for p in paulis))
         Y += delta * (R - X)
 
     return y_vec
