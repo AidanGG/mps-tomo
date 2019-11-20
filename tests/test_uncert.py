cf692c08a5eaa99b22bcfc86a3d72089712c0a61
Aidan Dang <dang@aidan.gg>
2019-11-20 13:52:26 +1100

    Add tests for depolarised W states

--- tests/test_uncert.py
+++ tests/test_uncert.py
@@ -3,7 +3,7 @@ from numpy.testing import assert_allclose
 from qiskit.quantum_info import random_density_matrix
 
 from mps_tomo.uncert import R_hat, iteration, pauli_proj
-from mps_tomo.utils import pauli_group
+from mps_tomo.utils import fidelity, pauli_group
 
 
 def test_pauli_proj():
@@ -19,7 +19,9 @@ def test_pauli_proj():
 
 def test_R_hat():
     TOL = 100 * np.spacing(np.complex128(1.0).real)
-    np.random.seed(7777)
+    SEED = 7777
+
+    np.random.seed(SEED)
 
     sigmas = [np.random.randn(4, 4) + 1j * np.random.randn(4, 4) for i in range(3)]
 
@@ -32,10 +34,10 @@ def test_R_hat():
     assert_allclose(R_hat(sigmas, 4), example, rtol=0.0, atol=TOL)
 
 
-def test_iteration():
+def test_iteration_pure_W():
     NUM_QUBITS = 5
     SEED = 7777
-    TOL = 0.15
+    TOL = 0.01
     K = 2
 
     np.random.seed(SEED)
@@ -43,7 +45,7 @@ def test_iteration():
     dim = 2 ** NUM_QUBITS
     state = np.zeros(dim, dtype=np.complex128)
     for i in range(NUM_QUBITS):
-        state[1 << i] = np.random.normal()
+        state[1 << i] = 1
     state /= np.linalg.norm(state)
 
     dens_mat = np.outer(state, state.conj())
@@ -63,5 +65,50 @@ def test_iteration():
     sigmas = (reduce(q) for q in range(NUM_QUBITS - K + 1))
 
     y_vec = iteration(K, sigmas, NUM_QUBITS, max_its=100, delta=0.1)
+    overlap = np.abs(state.conj() @ y_vec) ** 2
 
-    assert_allclose(np.abs(state.conj() @ y_vec) ** 2, 1, rtol=0.0, atol=TOL)
+    assert_allclose(overlap, 1, rtol=0.0, atol=TOL)
+
+
+def test_iteration_depolarising_W():
+    NUM_QUBITS = 5
+    SEED = 7777
+    TOL = 0.15
+    K = 2
+    DEPOLARISATION = 0.1
+
+    np.random.seed(SEED)
+
+    dim = 2 ** NUM_QUBITS
+    state = np.zeros(dim, dtype=np.complex128)
+    for i in range(NUM_QUBITS):
+        state[1 << i] = 1
+    state /= np.linalg.norm(state)
+
+    dens_mat = np.outer(state, state.conj())
+    dens_mat = (
+        DEPOLARISATION * (1 / dim) * np.eye(dim) + (1 - DEPOLARISATION) * dens_mat
+    )
+    purity = np.trace(dens_mat @ dens_mat)
+
+    def reduce(qubit):
+        left_size = 2 ** qubit
+        reduced_size = 2 ** K
+        right_size = np.size(dens_mat, axis=0) // (left_size * reduced_size)
+
+        reshaped = np.reshape(
+            dens_mat,
+            (left_size, reduced_size, right_size, left_size, reduced_size, right_size),
+        )
+
+        return np.einsum("aibajb->ij", reshaped)
+
+    sigmas = (reduce(q) for q in range(NUM_QUBITS - K + 1))
+
+    y_vec = iteration(K, sigmas, NUM_QUBITS, max_its=100, delta=0.1)
+    y_dens = np.outer(y_vec, y_vec.conj())
+
+    overlap = np.abs(state.conj() @ y_vec) ** 2
+    fid = fidelity(y_dens, dens_mat)
+
+    assert_allclose(overlap, 1, rtol=0.0, atol=TOL)
